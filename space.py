155ce4762882c2fc35029a76d0b8904b7ef55af0
Алексей <stormwww@yandex.ru>
2022-04-29 10:57:28 +0300

    создание пушки

--- space.py
+++ space.py
@@ -1,12 +1,13 @@
 import pygame
 import sys
+from gun import Gun
 
 def run():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Космо-защитники")
     bg_color = (0, 0, 0)
-
+    gun = Gun(screen)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +15,7 @@ def run():
                 sys.exit()
 
         screen.fill(bg_color)
+        gun.output()
         pygame.display.flip()
 
 run()
