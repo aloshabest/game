a3e781b86a20283c2011fffe7fd5ef363b0b1033
Алексей <stormwww@yandex.ru>
2022-04-29 10:55:57 +0300

    инициализация окна

---
+++ space.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Космо-защитники")
+    bg_color = (0, 0, 0)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+run()
